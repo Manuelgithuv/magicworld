d31b53533cb8986c757b5c6f606c09e5f0b99f43
Manuelgithuv <manuel.artero04@gmail.com>
2025-07-27 00:31:27 +0200

    Deploy website - based on bad1d69ac138d5e775a6b9c926defcc0ea1c6ea1

---
+++ assets/js/b17a37b5.825feca7.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs_site=self.webpackChunkdocs_site||[]).push([[8853],{748:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/magicworld/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
